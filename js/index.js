77dc66080023f6abe975ef16a402b33311659258
Steve0811 <case.SteveChan@gmail.com>
2023-02-15 09:25:12 +0800

    add tow video to media page

--- js/index.js
+++ js/index.js
@@ -1,35 +1,28 @@
-
-
 $(document).ready(function () {
-
-  let cursor = $('.cursor')
-  let banner = $('.banner')
+  let cursor = $('.cursor');
+  let banner = $('.banner');
   let header = $('header');
-  let bannerVideo = $('.banner--video')
-  let bannerLayer = $('.banner--layer')
+  let bannerVideo = $('.banner--video');
+  let bannerLayer = $('.banner--layer');
   banner.on('mousemove', (e) => {
-    let x = e.pageX, y = e.pageY;
+    let x = e.pageX,
+      y = e.pageY;
     cursor.css({
-      'left': x + 'px',
-      'top': y + 'px'
-    })
-    
-  })
-
-  
+      left: x + 'px',
+      top: y + 'px',
+    });
+  });
 
   banner.on('click', (e) => {
-    bannerVideo.toggleClass('show')
-    banner.toggleClass('show')
+    bannerVideo.toggleClass('show');
+    banner.toggleClass('show');
     bannerVideo.on('click', (e) => {
       e.stopPropagation();
-    })
-    bannerLayer.toggleClass('show')
+    });
+    bannerLayer.toggleClass('show');
 
-    cursor.toggle()
-  })
-
-
+    cursor.toggle();
+  });
 
   let y;
   let contentBoxes = document.querySelectorAll('.contentBox .content .box');
@@ -37,21 +30,18 @@ $(document).ready(function () {
 
   $(contentBoxes).each((index, value) => {
     contentBoxY.push(value.offsetTop);
-  })
-
+  });
 
   let moreBoxTitle = document.querySelectorAll('.contentBox .moreBoxTitle');
   let moreBox = document.querySelectorAll('.contentBox .moreBox');
 
-
-  let serviceBoxTitle = document.querySelectorAll('.contentBox .serviceBoxTitle');
+  let serviceBoxTitle = document.querySelectorAll(
+    '.contentBox .serviceBoxTitle'
+  );
   let serviceBox = document.querySelectorAll('.contentBox .serviceBox');
 
   let contact_contact = document.querySelectorAll('.contactBox .contact');
   let contact_title = document.querySelectorAll('.contactBox .title');
-
-
-
 
   $(window).on('scroll', function () {
     y = scrollY + window.innerHeight;
@@ -62,18 +52,28 @@ $(document).ready(function () {
       $(contentBoxes[i]).toggleClass('fade_in', y > contentBoxY[i]);
     }
 
-
-    $(moreBoxTitle).toggleClass('fade_in', y > $(moreBoxTitle).offset().top - 100);
+    $(moreBoxTitle).toggleClass(
+      'fade_in',
+      y > $(moreBoxTitle).offset().top - 100
+    );
     $(moreBox).toggleClass('fade_in', y > $(moreBoxTitle).offset().top - 100);
 
+    $(serviceBoxTitle).toggleClass(
+      'fade_in',
+      y > $(serviceBoxTitle).offset().top - 100
+    );
+    $(serviceBox).toggleClass(
+      'fade_in',
+      y > $(serviceBoxTitle).offset().top - 100
+    );
 
-    $(serviceBoxTitle).toggleClass('fade_in', y > $(serviceBoxTitle).offset().top - 100);
-    $(serviceBox).toggleClass('fade_in', y > $(serviceBoxTitle).offset().top - 100);
-
-    $(contact_contact).toggleClass('fade_in', y > $(contact_contact).offset().top - 100);
-    $(contact_title).toggleClass('fade_in', y > $(contact_contact).offset().top - 100);
-
-
-  })
-
-})
+    $(contact_contact).toggleClass(
+      'fade_in',
+      y > $(contact_contact).offset().top - 100
+    );
+    $(contact_title).toggleClass(
+      'fade_in',
+      y > $(contact_contact).offset().top - 100
+    );
+  });
+});
